b6a53694769c5c1ce84f4617466eb8ff2557bc00
Xety <emeric@xeta.io>
2017-04-30 21:28:22 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Xetaravel-CKEditor
-A wrapper to use @CKEditor with @Laravel
+> <h1 align="center">Xetaravel CKEditor</h1>
+>
+> This is a wrapper to use [CKEditor](https://github.com/ckeditor/ckeditor-releases) with Laravel.
+>
+> ## Installation
+>
+> ```
+> composer require xetaio/xetaravel-ckeditor
+> ```
+>
+> Import the CkeditorServiceProvider in your `config/app.php`:
+> ```
+> Xetaravel\Ckeditor\Providers\CkeditorServiceProvider::class
+> ```
+>
+> Publish the CKEditor to your public directory :
+> ```
+> php artisan vendor:publish --tag=ckeditor
+> ```
+>
+> ## Usage
+> ```
+><script src="/vendor/ckeditor/release/ckeditor.js"></script>
+>    <script>
+>        CKEDITOR.replace('name-go-here');
+>    </script>
+> ```
+>
+> ## Update
+> To update CKEditor to a new version, first run :
+> ```
+> composer update
+> ```
+> Then, delete the folder `public/vendor/release` and run the artisan command again :
+> ```
+> php artisan vendor:publish --tag=ckeditor
+> ```
+> **Note** : Do not put any custom configuration files in the `public/vendor/release` directory. Instead put them in the `public/vendor` directory or somewhere else.
---
+++ app/Providers/CkeditorServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+namespace Xetaravel\Ckeditor\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CkeditorServiceProviders extends ServiceProvider
+{
+    /**
+     * Publish the CKEditor to the public directory.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $basePath = base_path('vendor/ckeditor/release');
+        $publicPath = 'vendor/ckeditor/ckeditor';
+
+        $this->publishes(
+            [
+                $basePath . '/ckeditor.js' => public_path($publicPath . '/ckeditor.js'),
+                $basePath . '/config.js' => public_path($publicPath . '/config.js'),
+                $basePath . '/styles.js' => public_path($publicPath . '/styles.js'),
+                $basePath . '/contents.css' => public_path($publicPath . '/contents.css'),
+                $basePath . '/adapters' => public_path($publicPath . '/adapters'),
+                $basePath . '/lang' => public_path($publicPath . '/lang'),
+                $basePath . '/skins' => public_path($publicPath . '/skins'),
+                $basePath . '/plugins' => public_path($publicPath . '/plugins'),
+            ],
+            'ckeditor'
+        );
+    }
+}
